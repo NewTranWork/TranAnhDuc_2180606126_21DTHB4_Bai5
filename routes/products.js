6c23da679eb374034555c2be258f810c23c14c33
NewTranWork <143317056+NewTranWork@users.noreply.github.com>
2025-03-08 19:37:49 +0700

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -8,7 +8,7 @@ let BuildQueies = require('../Utils/BuildQuery')
 /* GET users listing. */
 router.get('/', async function(req, res, next) {
   let queries = req.query;
-  let products = await productSchema.find(BuildQueies.QueryProduct(queries));
+  let products = await productSchema.find(BuildQueies.QueryProduct(queries)).populate("categoryID");
   res.send(products);
 });
 router.get('/:id', async function(req, res, next) {
@@ -36,6 +36,32 @@ router.post('/', async function(req, res, next) {
   })
   await newProduct.save()
   res.send(newProduct);
+});
+router.put('/:id', async function(req, res, next) {
+  try {
+    let body = req.body;
+    let product = await productSchema.findByIdAndUpdate(req.params.id,
+      body,{new:true});
+    res.status(200).send({
+      success:true,
+      data:product
+    });
+  } catch (error) {
+    res.status(404).send({
+      success:fail,
+      message:error.message
+    })
+  }
+  // let body = req.body;
+  // console.log(body);
+  // let newProduct = new productSchema({
+  //   productName: body.productName,
+  //   price: body.price,
+  //   quantity: body.quantity,
+  //   categoryID: body.category
+  // })
+  // await newProduct.save()
+  // res.send(newProduct);
 });
 
 
